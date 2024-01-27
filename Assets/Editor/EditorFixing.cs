27bf40d4d1f4afe0842162ece2a12803ff365523
billsaferr@gmail.com <>
2024-01-27 18:09:08 +0200

    started putting together ui

--- Assets/Editor/EditorFixing.cs
+++ Assets/Editor/EditorFixing.cs
@@ -3,6 +3,17 @@ using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEditor.Build;
 
+static class EditorMenus
+{
+    // taken from: http://answers.unity3d.com/questions/282959/set-inspector-lock-by-code.html
+    [MenuItem("Tools/Toggle Inspector Lock %l")] // Ctrl + L
+    static void ToggleInspectorLock()
+    {
+        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+        ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+}
+
 public class EditorFixing : IPreprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }
