e1d1c50bdfc4cad44f51494cc5aa62254282c35d
billsaferr@gmail.com <>
2024-03-20 15:41:19 +0200

    прогресуєм💪💪💪💪💪💪💪💪

---
+++ Assets/хуйня/ToCameraRotator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToCameraRotator : MonoBehaviour
+{
+    Camera mainC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainC = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(mainC.transform.position);
+    }
+}
